2c18c95e075910af9481e4bd2c14646e237248cd
Lee <44310445+lx200916@users.noreply.github.com>
2022-06-12 16:16:37 +0800

    Fix Bugs

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
-const { ipcMain, BrowserWindow } = require("electron")
-const path = require("path")
-const { spawn } = require('child_process');
-const net = require('net');
-const PIPE_NAME = '\\\\.\\Pipe\\lyrics_pipe';
+const { ipcMain, BrowserWindow } = require("electron");
+const path = require("path");
+const { spawn } = require("child_process");
+const net = require("net");
+const PIPE_NAME = "\\\\.\\Pipe\\lyrics_pipe";
 
-module.exports =class DesktopLyricsPluginService{
-    win;
-    constructor(env) {
-        // Define plugin enviornment within the class
-        this.env = env
-    }
-    onReady(win) {
-        console.log("=== Backend Plugin Loaded ===")
-        this.child = spawn(`${path.join(this.env.dir,"untitled13.exe")} > ${path.join(this.env.dir,"plugin.log")} 2>&1`,[],{shell:true});
-        // // Setting up an ipcMain channel for front end to communicate with
-        // ipcMain.handle("plugin.frontendComm", (event, message) => {
-        //     // Print out what the front end says
-        //     console.debug(`Frontend says: ${message}`)
-        //     // Get the main window and send a messsage to it
-        //     this.env.utils.getWindow().webContents.send("plugin.backendComm", "Hello from the backend!")
-        // })
-    }
-    onRendererReady(win) {
-        console.debug("Renderer Ready Called")
-        // Load the frontend plugin
-        this.env.utils.loadJSFrontend(path.join(this.env.dir, "index.frontend.js"));
-        if(!this.child || this.child.killed) {
-            this.child = spawn(`${path.join(this.env.dir,"untitled13.exe")} > ${path.join(this.env.dir,"plugin.log")} 2>&1`,[],{shell:true});
-        }
-        this.pipeClient= net.createConnection( PIPE_NAME, () => {
-            console.log('connected to server!!');
-          });
-          
-          this.pipeClient.on('end', () => {
-            console.log('disconnected from server');
-            
-          });
-          ipcMain.on("MDesktopLyricsUpdate", (event, args) => {
-            // console.log("MDesktopLyricsUpdate", args);
-            // this.pipeClient.write(args);
-        
-          })
-          ipcMain.on("MDesktopLyricsLineUpdate", (event, args) => {
-            //   console.log("MDesktopLyricsLineUpdate", args);
-            this.pipeClient.write(args);
-            
-          })
+module.exports = class DesktopLyricsPluginService {
+  win;
+  constructor(env) {
+    // Define plugin enviornment within the class
+    this.env = env;
+  }
+  onReady(win) {
+    console.log("=== Backend Plugin Loaded ===");
+    this.child = spawn(
+      `${path.join(this.env.dir, "untitled13.exe")} > ${path.join(
+        this.env.dir,
+        "plugin.log"
+      )} 2>&1`,
+      [],
+      { shell: true }
+    );
+  }
+  onRendererReady(win) {
+    console.debug("Renderer Ready Called");
 
-        
-
+    // Load the frontend plugin
+    this.env.utils.loadJSFrontend(path.join(this.env.dir, "index.frontend.js"));
+    if (!this.child || this.child.killed) {
+      this.child = spawn(
+        `${path.join(this.env.dir, "untitled13.exe")} > ${path.join(
+          this.env.dir,
+          "plugin.log"
+        )} 2>&1`,
+        [],
+        { shell: true }
+      );
     }
-    onBeforeQuit(){
-        this.child.kill();
-    }
+    console.log("PID:", this.child.pid);
+    this.pipeClient = net.createConnection(PIPE_NAME, () => {
+      console.log("connected to server!!");
+    });
 
-}
+    this.pipeClient.on("end", () => {
+      console.log("disconnected from server");
+    });
+    ipcMain.on("MDesktopLyricsUpdate", (event, args) => {
+      // console.log("MDesktopLyricsUpdate", args);
+      // this.pipeClient.write(args);
+    });
+    ipcMain.on("MDesktopLyricsLineUpdate", (event, args) => {
+      //   console.log("MDesktopLyricsLineUpdate", args);
+      this.pipeClient.write(args);
+    });
+  }
+  onBeforeQuit() {
+    // this.child.kill();
+    console.log("kill", this.child.pid);
+    spawn("taskkill", ["/pid", this.child.pid, "/F", "/T"], { shell: true });
+  }
+};
